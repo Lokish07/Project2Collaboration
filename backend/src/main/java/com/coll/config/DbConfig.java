17b000c734648c96bc8867ec520f14f20a347b77
J.Lokish <J.Lokish@LAPTOP-OLJQ6KGD>
2019-04-10 21:16:38 +0530

    first commit

---
+++ backend/src/main/java/com/coll/config/DbConfig.java
@@ -0,0 +1,56 @@
+package com.coll.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.coll.model.Blog;
+
+@Configuration
+@ComponentScan("com.niit")
+@EnableTransactionManagement
+public class DbConfig {
+
+	@Bean(name="dataSource")
+	public DataSource getDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+		dataSource.setUrl("jdbc:oracle:thin:@localhost:1522:xe");
+		dataSource.setUsername("SYSTEM");
+		dataSource.setPassword("Admin12345");
+		System.out.println("---Data Source created---");
+		return dataSource;
+	}
+
+	@Bean(name = "sessionFactory")
+	public SessionFactory getSessionFactory() {
+		Properties hibernateProp = new Properties();
+		hibernateProp.put("hibernate.hbmddl2.auto", "update");
+		hibernateProp.put("hibernate.dialect", "org.hibernate.dialect.Oracle10gDialect");
+
+		LocalSessionFactoryBuilder factoryBuilder = new LocalSessionFactoryBuilder(getDataSource());
+		factoryBuilder.addProperties(hibernateProp);
+
+		factoryBuilder.addAnnotatedClass(Blog.class);
+		
+
+		SessionFactory sessionFactory = factoryBuilder.buildSessionFactory();
+		System.out.println("---SessionFactory created---");
+		return sessionFactory;
+	}
+
+	@Bean
+	public HibernateTransactionManager getHibernateTransactionManager(SessionFactory sessionFactory) {
+		System.out.println("---TransactionManager created---");
+		return new HibernateTransactionManager(sessionFactory);
+	}
+}
---
+++ backend/src/test/java/com/coll/test/DbConfigTest.java
@@ -0,0 +1,12 @@
+package com.coll.test;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class DbConfigTest {
+	public static void main(String arg[]) {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		context.scan("com.coll");
+		context.refresh();
+		
+	}
+}
