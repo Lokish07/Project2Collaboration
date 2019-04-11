2d14594c9d5ec41fe73d28516a980317d1d10161
J.Lokish <J.Lokish@connect.facebook.com>
2019-04-11 09:01:33 +0530

    Forum model done

---
+++ backend/src/main/java/com/coll/model/Forum.java
@@ -0,0 +1,73 @@
+package com.coll.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+@Table
+public class Forum {
+	@Id
+	@GeneratedValue
+	private int forumId;
+	private String forumTitle;
+	private String forumContent;
+	private String username;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss a")
+	private Date createdDate;
+	private String status;
+
+	public int getForumId() {
+		return forumId;
+	}
+
+	public void setForumId(int forumId) {
+		this.forumId = forumId;
+	}
+
+	public String getForumTitle() {
+		return forumTitle;
+	}
+
+	public void setForumTitle(String forumTitle) {
+		this.forumTitle = forumTitle;
+	}
+
+	public String getForumContent() {
+		return forumContent;
+	}
+
+	public void setForumContent(String forumContent) {
+		this.forumContent = forumContent;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
