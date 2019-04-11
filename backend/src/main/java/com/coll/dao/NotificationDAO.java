e42a7cd82c09579fe669c07979ac7bba8c5f2e6d
J.Lokish <J.Lokish@192.168.1.22>
2019-04-11 18:59:40 +0530

    Notification controller

---
+++ backend/src/main/java/com/coll/dao/NotificationDAO.java
@@ -0,0 +1,20 @@
+package com.coll.dao;
+
+import java.util.List;
+
+import com.coll.model.Notification;
+
+public interface NotificationDAO
+{
+	public boolean addNotification(Notification notification);
+
+	public boolean deleteNotification(Notification notification);
+
+	public boolean markNotificationAsRead(int notificationId);
+
+	public boolean markNotificationAsUnread(int notificationId);
+
+	public Notification getNotification(int notificationId);
+
+	public List<Notification> getNotificationList(String username);
+}
